ee59fbd14b39adc2c45937d346e5f855808e8fea
lukas braun <lukas.braun@mailbox.org>
2020-05-16 16:43:24 +0200

    Add delete-db script. Chg docker image script

--- docker-delete-all-images.sh
+++ docker-delete-all-images.sh
@@ -3,6 +3,7 @@
 echo 'Remove everything - even database-volumes!'
 echo 'Continue in 5 seconds'
 sleep 5
-sudo docker container prune \
-        && sudo docker system prune --volumes \
-        && sudo docker image rm $(sudo docker image ls -aq)
+sudo docker container prune
+sudo docker container rm $(sudo docker container ls -aq) --force
+sudo docker system prune --volumes
+sudo docker image rm $(sudo docker image ls -aq) --force
