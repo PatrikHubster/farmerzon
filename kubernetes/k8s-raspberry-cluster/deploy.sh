3677094af274d598cd617a746a182bf149b43332
patrikhuber <pahub@icloud.com>
2020-12-05 21:26:24 +0100

    add as well other databases

--- kubernetes/k8s-raspberry-cluster/deploy.sh
+++ kubernetes/k8s-raspberry-cluster/deploy.sh
@@ -2,12 +2,18 @@
 microk8s kubectl apply -f postgres-pv.yml
 
 # persistent volume claims
+microk8s kubectl apply -f farmerzon-address-pvc.yml
+microk8s kubectl apply -f farmerzon-articles-pvc.yml
 microk8s kubectl apply -f farmerzon-authentication-pvc.yml
 
 # config maps
+microk8s kubectl apply -f farmerzon-address-cm.yml
+microk8s kubectl apply -f farmerzon-articles-cm.yml
 microk8s kubectl apply -f farmerzon-authentication-cm.yml
 
 # stateful sets
+microk8s kubectl apply -f farmerzon-address-sts.yml
+microk8s kubectl apply -f farmerzon-articles-sts.yml
 microk8s kubectl apply -f farmerzon-authentication-sts.yml
 
 # services
--- kubernetes/k8s-raspberry-cluster/tear-down.sh
+++ kubernetes/k8s-raspberry-cluster/tear-down.sh
@@ -2,12 +2,18 @@
 microk8s kubectl delete svc farmerzon-authentication-np
 
 # stateful sets
+microk8s kubectl delete sts farmerzon-address-sts
+microk8s kubectl delete sts farmerzon-articles-sts
 microk8s kubectl delete sts farmerzon-authentication-sts
 
 # config maps
+microk8s kubectl delete cm farmerzon-address-cm
+microk8s kubectl delete cm farmerzon-articles-cm
 microk8s kubectl delete cm farmerzon-authentication-cm
 
 # persistent volume claims
+microk8s kubectl delete pvc farmerzon-address-pvc
+microk8s kubectl delete pvc farmerzon-articles-pvc
 microk8s kubectl delete pvc farmerzon-authentication-pvc
 
 # persistent volumes
